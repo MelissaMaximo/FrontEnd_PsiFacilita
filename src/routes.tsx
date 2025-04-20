cb577f9b22591e3af0f7cc5668c42ddf8a3cad1d
MelissaMaximo <129124909+MelissaMaximo@users.noreply.github.com>
2025-04-20 20:54:54 -0300

    Corrigindo Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,8 @@ import Patients from './pages/Patients';
 import Documents from './pages/Documents';
 import Settings from './pages/Settings';
 import MainLayout from './components/layout/MainLayout/MainLayout';
-import { Playground } from '../src/playground/playground';
 
 const AppRoutes: React.FC = () => {
-  // Estado para o sidebar (agora sendo gerenciado aqui)
-  const [sidebarOpen, setSidebarOpen] = React.useState(false);
-
   return (
     <Routes>
       {/* Rotas sem layout */}
@@ -20,16 +16,13 @@ const AppRoutes: React.FC = () => {
       <Route path="/login" element={<Login />} />
       
       {/* Rotas com layout */}
-      <Route element={<MainLayout sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />}>
+      <Route element={<MainLayout children={undefined} />}>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/appointments" element={<Appointments />} />
         <Route path="/patients" element={<Patients />} />
         <Route path="/documents" element={<Documents />} />
         <Route path="/settings" element={<Settings />} />
       </Route>
-
-      {/* Rota do Playground - sem layout */}
-      <Route path="/playground" element={<Playground />} />
     </Routes>
   );
 };
